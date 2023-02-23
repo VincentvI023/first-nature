2e6d4f5bd3a6f9092121fa79ac08370e1fdb4da9
Vincent van Ingen <info@vincentvaningen.nl>
2023-02-23 11:13:08 +0100

    Add footer in ACF

--- footer.php
+++ footer.php
@@ -3,13 +3,15 @@
     <div class="container">
       <div class="row">
         <div class="col-md-4">
-          <h4>06-12345678</h4>
+          <?php $phone = get_field('telefoonnummer', 'option'); ?>
+          <h4><a href="tel:<?php echo antispambot( $phone ); ?>"><?php echo antispambot( $phone ); ?></a></h4>
         </div>
         <div class="col-md-4">
           <h4>- Contact -</h4>
         </div>
         <div class="col-md-4">
-          <h4>gert-willem@firstnature.nl</h4>
+          <?php $email = get_field('emailadres', 'option');?> 
+          <h4><a href="mailto:<?php echo antispambot( $email ); ?>"><?php echo antispambot( $email ); ?></a></h4>
         </div>
       </div>
     </div>
@@ -19,10 +21,25 @@
       <div class="row">
         <div class="col-md-6">
           <div class="extra-info">
-            <p>SKJ, Stichting Kwaliteitsregister Jeugd (nr: 130002670)</p> 
-            <p>AGB code vestiging: 94063438</p> 
-            <p>AGB code behandelaar: 94103322</p> 
-            <p>KVK: 68662769</p>
+            <?php 
+              if( have_rows('footer_info', 'option') ):
+                  while ( have_rows('footer_info', 'option') ) : the_row();
+                      $item_type = get_sub_field('item_type', 'option');
+                      $item = get_sub_field('item', 'option');
+                      $item_link = get_sub_field('item_link', 'option'); ?>
+
+                      <p>
+                        <?php if(!empty($item_link)) : ?>
+                            <a href="<?php echo esc_url($item_link); ?>">
+                                <?php echo esc_html($item_type); ?>: <?php echo esc_html($item); ?>
+                            </a>
+                        <?php else: ?>
+                            <?php echo esc_html($item_type); ?>: <?php echo esc_html($item); ?>
+                        <?php endif; ?>
+                      </p>
+
+              <?php endwhile;
+              endif; ?>
             <div class="green-line-footer"></div>
           </div>
         </div>
