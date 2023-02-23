938b7bf10a716f3c6f40c907f3ff598171d65c40
Vincent van Ingen <info@vincentvaningen.nl>
2023-02-23 14:14:32 +0100

    Add animation on title link

--- js/main.js
+++ js/main.js
@@ -49,3 +49,38 @@ onYouTubeIframeAPIReady = function () {
 		}
 	});
 };
+
+// Button underline animation
+document.addEventListener('DOMContentLoaded', function () {
+	// Detect when the element is in the viewport
+	var animation_elements = document.querySelectorAll('.underline-animation');
+	var window_height = window.innerHeight;
+
+	function check_if_in_view() {
+		var window_top_position = window.pageYOffset;
+		var window_bottom_position = (window_top_position + window_height);
+
+		for (var i = 0; i < animation_elements.length; i++) {
+			var element = animation_elements[i];
+			var element_height = element.offsetHeight;
+			var element_top_position = element.offsetTop;
+			var element_bottom_position = (element_top_position + element_height);
+
+			// If the element is in the viewport, add the "visible" class after a 2 second delay
+			if ((element_bottom_position >= window_top_position + 200) &&
+				(element_top_position <= window_bottom_position)) {
+				setTimeout(function () {
+					element.classList.add('visible');
+				}, 2000);
+			}
+		}
+	}
+
+	window.addEventListener('scroll', check_if_in_view);
+	window.addEventListener('resize', function () {
+		window_height = window.innerHeight;
+	});
+
+	// Check if any elements are already in view on page load
+	check_if_in_view();
+});
